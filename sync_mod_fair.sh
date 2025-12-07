70c40af233193c084699a0ce345ddaf74bad5894
Shanbo Zhang <EscapistArcadia@outlook.com>
2025-12-07 11:53:10 -0600

    Added script for fair

---
+++ sync_mod_fair.sh
@@ -0,0 +1,6 @@
+rm -rf ./gemm_sm_fair_mod/gemm_sm_rtl.v
+rm -rf ./gemm_sm_fair_mod/v_rtl
+scp    shanboz2@denali.cs.illinois.edu:/scratch/shanboz2/esp_accel/accelerators/stratus_hls/gemm_sm_stratus/hw/hls-work-virtexup/bdw_work/modules/gemm_sm/BASIC_DMA64_FAIR/gemm_sm_rtl.v ./gemm_sm_fair_mod/gemm_sm_rtl.v
+scp -r shanboz2@denali.cs.illinois.edu:/scratch/shanboz2/esp_accel/accelerators/stratus_hls/gemm_sm_stratus/hw/hls-work-virtexup/bdw_work/modules/gemm_sm/BASIC_DMA64_FAIR/v_rtl ./gemm_sm_fair_mod/v_rtl
+
+./area.sh -l ./OpenROAD/TimerCalibration/Free45PDK/gscl45nm.lib -v ./gemm_sm_fair_mod/ -t gemm_sm -o gemm_sm_fair_mod.log
